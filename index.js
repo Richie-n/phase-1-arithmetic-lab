ef42bf4f0a4f35f213b3c83aec9a23efaa737f48
Richard <ngangarichard335@gmail.com>
2024-03-22 16:10:19 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+
+const random = 1
+
+const num3 = 24
+const num4 = 20
+const mod = num3 - num4
+const max = Math.max(1, 2, 3, 4, 20)
